e1cdd32e900e053805813c54654a8d845fb0cc67
zw5sx@virginia.edu <Walter@d-172-25-99-198.dhcp.virginia.edu>
2017-04-08 15:59:14 -0400

    Update

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ var	app 		= express();
 var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
-  password : 'project'
+  password : 'project',
+  database : 'Project'
 });
 connection.connect(function(err) {
   if (err) {
@@ -24,13 +25,72 @@ app.set("view engine", "ejs");
 
 //Root Routes
 app.get("/", function(req, res){
-	res.send("Welcome to Database project");
+	res.redirect("/Welcome");
 });
 //index Route
 app.get("/Welcome", function(req, res){
 	res.render("index");
 });
 
+app.get("/signup", function(req, res){
+	res.render("SignUp");
+});
+app.get("/signin", function(req, res){
+	res.render("SignIn");
+});
+app.get("/Car/new", function(req, res){
+	res.render("newCar")
+});
+
+app.get("/Car/:id", function(req, res){
+	res.render("show");
+});
+app.post("/Car", function(req,res){
+	newCar = {
+		Brand : req.body.Brand,
+		Model : req.body.Model,
+		Year  : req.body.Year,
+		Type  : req.body.Type,
+		Title : req.body.Title,
+		Price : req.body.Price,
+		Mileage : req.body.Mileage,
+		Image : req.body.Image,
+		Description : req.body.Description
+	};
+	connection.query("show databases", function(err, results, fields){
+		if(err) throw err;
+		else {
+			res.send(newCar);
+		}
+	});
+});
+
+app.post("/signin", function(req, res){
+	var Username = req.body.Username;
+	var Password = req.body.Password;
+	connection.query('select Password from User where Username = "' + Username +'"' , function(err, results, fields){
+		if(err) throw err;
+		else {
+			if (Password === results[0].Password){
+				res.send("Log In successfully!");
+			} else {
+				res.send(results[0].Password);
+			}
+		}
+	});
+});
+
+app.post("/NewUser", function(req, res){
+	var Username = req.body.Username;
+	var Password = req.body.Password;
+	var Phone = req.body.Phone;
+	var Email = req.body.Email;
+	connection.query('insert into User (Username, Password, Phone, Email) values ("' + Username + '", "' + Password + '", "' + Phone + '", "' + Email + '")',
+	function (err, results, fields) {
+	    if (err) throw err;
+	    else res.send('success');
+	});
+});
 
 
 

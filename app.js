5bf3a5ad838f1e4ef66180b40805e6515d859a74
ekoyama <ek4ks@virginia.edu>
2017-04-22 15:07:18 -0400

    Signing in working.

--- app.js
+++ app.js
@@ -1,24 +1,43 @@
 var express 	= require("express"),
 	mysql   	= require("mysql"),
+	session = require('express-session'),
 	bodyParser 	= require("body-parser");
+	
+var MySQLStore = require('express-mysql-session')(session);
 
-var	app 		= express();
-
-//Connect to mySQL Database
-var connection = mysql.createConnection({
+var options = {
   host     : 'localhost',
-  user     : 'root',
+  user	   : 'dbproject',
   password : 'project',
   database : 'Project'
-});
+  //  host     : 'localhost',
+//  user     : 'root',
+//  password : 'project',
+//  database : 'Project'
+};
+
+//Connect to mySQL Database
+var connection = mysql.createConnection(options);
+var sessionStore = new MySQLStore({}, connection);
+
+var	app = express();
+
+app.use(session({
+    key: 'session_cookie_name',
+    secret: 'session_cookie_secret',
+    store: sessionStore,
+    resave: true,
+    saveUninitialized: true
+}));
+
 connection.connect(function(err) {
   if (err) {
     console.error('error connecting: ' + err.stack);
     return;
   }
- 
   console.log('connected as id ' + connection.threadId);
 });
+
 //set up bodyParser and view engine
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
@@ -29,15 +48,28 @@ app.get("/", function(req, res){
 });
 //index Route
 app.get("/Welcome", function(req, res){
-	res.render("index");
+	console.log(req.session.username)
+	res.render("index", {username: req.session.username, welcome: true});
 });
 
 app.get("/signup", function(req, res){
-	res.render("SignUp");
+	res.render("signUp");
 });
-app.get("/signin", function(req, res){
-	res.render("SignIn");
+
+app.get("/login", function(req, res){
+	if (typeof req.session.username == 'undefined') {
+		res.render("login");
+	}
+	else {
+		res.redirect("/Welcome");
+	}
 });
+
+app.get("/logout", function(req, res){
+	delete req.session.username;
+	res.redirect("/Welcome");
+});
+
 app.get("/Car/new", function(req, res){
 	res.render("newCar")
 });
@@ -65,35 +97,40 @@ app.post("/Car", function(req,res){
 	});
 });
 
-app.post("/signin", function(req, res){
-	var Username = req.body.Username;
-	var Password = req.body.Password;
-	connection.query('select Password from User where Username = "' + Username +'"' , function(err, results, fields){
-		if(err) throw err;
+app.post("/login", function(req, res){
+	var username = req.body.username;
+	var password = req.body.password;
+	connection.query('select password from User where username = "' + username + '"' , function(err, results, fields){
+		if(err) 
+			throw err;
 		else {
-			if (Password === results[0].Password){
-				res.send("Log In successfully!");
+			if (password === results[0].password) {
+				req.session.username = username;
+				res.redirect("/Welcome");
 			} else {
-				res.send(results[0].Password);
+				res.render("login", {error: "Username or password is incorrect."});
 			}
 		}
 	});
 });
 
 app.post("/NewUser", function(req, res){
-	var Username = req.body.Username;
-	var Password = req.body.Password;
-	var Phone = req.body.Phone;
-	var Email = req.body.Email;
-	connection.query('insert into User (Username, Password, Phone, Email) values ("' + Username + '", "' + Password + '", "' + Phone + '", "' + Email + '")',
+	var username = req.body.username;
+	var password = req.body.password;
+	var password2 = req.body.password2;
+	var phone = req.body.phone;
+	var email = req.body.email;
+	if (password != password2) {
+		res.render("signUp", {error: "Password does not match"});
+	}
+	
+	connection.query('insert into User (username, password, phone, email) values ("' + username + '", "' + password + '", "' + phone + '", "' + email + '")',
 	function (err, results, fields) {
 	    if (err) throw err;
+		//password does not match
 	    else res.send('success');
 	});
 });
-
-
-
 
 //Listen on localhost:8000
 app.listen(8000, 'localhost', function(){
